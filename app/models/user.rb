639a6fadd46f069ad3bf43a5e83ced81c180924e
Elvis Serrão <elvis.serrao1@gmail.com>
2020-08-30 18:27:34 -0300

    Cria validações para os campos do objeto user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable, :trackable
 
+  validates_presence_of :first_name, on: :create, message: "não pode ficar em branco"
+  validates_presence_of :last_name, on: :create, message: "não pode ficar em branco"
+  validates_presence_of :age, on: :create, message: "não pode ficar em branco"
+
 end
