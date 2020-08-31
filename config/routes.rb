28516ab55dc410955a5c94d2c609d56219c67013
Elvis Serrão <elvis.serrao1@gmail.com>
2020-08-30 23:22:11 -0300

    Cria rotas para cartões

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
+  resources :cards, only: [:new, :create, :edit, :update, :destroy]
   get 'cards', action: :cards, controller: 'home'
   get 'invoices', action: :invoices, controller: 'home'
 
